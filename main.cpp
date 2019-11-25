9c70f285f5b68deb73b1679c6abec09c4d6c2050
Subtora <subtora@github.com>
2019-11-25 11:07:18 -0700

    final

--- main.cpp
+++ main.cpp
@@ -2,12 +2,48 @@
 #include <iostream>
 #include <stack>
 
+std::stack <int> a, b, c;
+int size = 5;
+int count = 0;
+
+//METHODS
+
+void move(std::stack<int>& first, std::stack<int>& second);
+
+bool checkValidity(std::stack<int> a, std::stack<int> b);
+
+void show(std::stack<int> s);
+
+void showAll(std::stack<int> a, std::stack<int> b, std::stack<int> c);
+
+void initialize(std::stack<int>& s);
+
+
+//DEFINITIONS
+
 void move(std::stack<int>& first, std::stack<int>& second){
+  if(checkValidity(first, second)){
     second.push(first.top());
     first.pop();
+    std::cout <<"success\n";
+  }else{
+    std::cout <<"fail\n";
+  }
+  count++;
+  showAll(a, b, c);
+}
+
+bool checkValidity(std::stack<int> a, std::stack<int> b){
+  bool valid = true;
+  while(!b.empty()){
+        if(b.top() < a.top()) valid = false;
+        b.pop();
+  }
+  return valid;
 }
 
 void show(std::stack<int> s){
+  std::cout << "stack: ";
     while(!s.empty()){
         std::cout << s.top();
         s.pop();
@@ -16,34 +52,38 @@ void show(std::stack<int> s){
 }
 
 void showAll(std::stack<int> a, std::stack<int> b, std::stack<int> c){
+  std::cout << "\nRESULT:\n";
     show(a);
     show(b);
     show(c);
+    std::cout << "\n";
 }
 
-void initialize(std::stack<int>& a){
-    a.push(5);
-    a.push(4);
-    a.push(3);
-    a.push(2);
-    a.push(1);
+void initialize(int size, std::stack<int>& s){
+    for (int i = size; i > 0; i--) {
+      s.push(i);
+    }
 }
 
+
+void toh(int size, std::stack<int> &from, std::stack<int> &to, std::stack<int> &aux) {
+  if (size == 1) {
+    move(from, to);
+    return;
+  }
+  toh(size - 1, from, aux, to);
+  move(from, to);
+  toh(size - 1, aux, to, from);
+}
 
 int main()
 {
-    std::stack <int> a, b, c;
 
-    initialize(a);
+    initialize(size, a);
 
+    toh(size, a, c, b);
 
-    move(a, b);
-    move(b, c);
-    move(a, b);
-
-
-    showAll(a, b, c);
-
+    std::cout << count << "\n";
 
  return 0;
 }
